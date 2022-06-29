c8c0583489e865c631429cd7b1645447581e2469
emmy <52672675+emmettgb@users.noreply.github.com>
2022-06-29 13:16:40 -0500

    updated docs

--- src/ToolipsBase64.jl
+++ src/ToolipsBase64.jl
@@ -3,6 +3,16 @@ using Toolips
 using ToolipsSession
 using Base64
 
+"""
+**Base64 Interface**
+### base64img(path::String) -> ::Component
+------------------
+Creates a Base64 image component from an image file.
+#### example
+```
+
+```
+"""
 function base64img(path::String)
       filetype = split(path, ".")[2]
       raw = read(path)
@@ -14,6 +24,16 @@ function base64img(path::String)
       img("myimg", src = "data:image/$filetype;base64," * mysrc)
 end
 
+"""
+**Base64 Interface**
+### base64img(raw::String, filetype::String = "png") -> ::Component
+------------------
+Creates a Base64 image component from a raw string of image data.
+#### example
+```
+
+```
+"""
 function base64img(raw::String, filetype::String = "png")
       io = IOBuffer();
       b64 = Base64.Base64EncodePipe(io)
@@ -23,6 +43,17 @@ function base64img(raw::String, filetype::String = "png")
       img("myimg", src = "data:image/$filetype;base64," * mysrc)
 end
 
+"""
+**Base64 Interface**
+### base64img(raw::String, filetype::String = "png") -> ::Component
+------------------
+Creates a Base64 image component from any type shown with the image/png mime.
+For example, a plot which only shows as a png.
+#### example
+```
+
+```
+"""
 function base64img(raw::Any, filetype::String = "png")
       io = IOBuffer();
       b64 = Base64.Base64EncodePipe(io)
@@ -32,6 +63,16 @@ function base64img(raw::Any, filetype::String = "png")
       img("myimg", src = "data:image/$filetype;base64," * mysrc)
 end
 
+"""
+**Base64 Interface**
+### update_base64!(cm::ComponentModifier, name::String, raw::Any, filetype::String = "png") -> ::Component
+------------------
+Updates a given img component by name with the raw source as Base64
+#### example
+```
+
+```
+"""
 function update_base64!(cm::ComponentModifier, name::String, raw::Any,
       filetype::String = "png")
       io = IOBuffer();
@@ -42,11 +83,31 @@ function update_base64!(cm::ComponentModifier, name::String, raw::Any,
       cm[name] = "src" => "data:image/$filetype;base64," * mysrc
 end
 
+"""
+**Base64 Interface**
+### update_base64!(cm::ComponentModifier, name::Component, raw::Any, filetype::String = "png") -> ::Component
+------------------
+Updates a given img component with the raw source as Base64
+#### example
+```
+
+```
+"""
 function update_base64!(cm::ComponentModifier, s::Component, raw::Any,
       filetype::String = "png")
       update_base64!(cm, s.name, raw, filetype)
 end
 
+"""
+**Base64 Interface**
+### update_base64!(cm::ComponentModifier, name::String, raw::String, filetype::String = "png") -> ::Component
+------------------
+Updates a given img component by name with the raw source as Base64
+#### example
+```
+
+```
+"""
 function update_base64!(cm::ComponentModifier, name::String, raw::String,
       filetype::String = "png")
       io = IOBuffer();
@@ -57,6 +118,16 @@ function update_base64!(cm::ComponentModifier, name::String, raw::String,
       cm[name] = "src" => "data:image/$filetype;base64," * mysrc
 end
 
+"""
+**Base64 Interface**
+### update_base64!(cm::ComponentModifier, name::Component, raw::String, filetype::String = "png") -> ::Component
+------------------
+Updates a given img component with the raw source as Base64
+#### example
+```
+
+```
+"""
 function update_base64!(cm::ComponentModifier, s::Component, raw::String,
       filetype::String = "png")
       update_base64!(cm, s.name, raw, filetype)
